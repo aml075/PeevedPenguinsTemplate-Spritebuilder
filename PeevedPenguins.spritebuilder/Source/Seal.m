a8e7b2da9360cfc232db677f9b05beceabc786ae
Richard Tubbs <richard@sh-tsquib-lt7.stretchline.local>
2014-07-03 16:55:01 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Richard Tubbs on 03/07/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal Created");
+    }
+    
+    return self;
+}
+
+@end
